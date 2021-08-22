a706000999a41ac6dff251667921f2295241327f
asif <masifabid@outlook.com>
2021-08-22 12:35:35 +0530

    .

--- app.js
+++ app.js
@@ -3,17 +3,27 @@
 //alert("This works man")
 
 //var username = prompt("Give me username")
+//console.log(username)
 //alert ("this script works bro " + username)
+
+console.log(document)
+var heading = document.querySelector(".heading")
+console.log(heading.textContent)
+// heading.textContent = username
+var output = document.querySelector("#textOutput")
+console.log(output.textContent)
 
 var btnTranslate = document.querySelector("#btnTranslate");
 
-//button is not working
-var textInput = document.querySelector("#textInput");
+  var textInput = document.querySelector("#textInput");
 
-btnTranslate.addEventListener("onclick", function () {
-    console.log("Clicked");
-    console.log("input", textInput.value);
+    btnTranslate.addEventListener("click", function () {
+      var input = textInput.value
+      output.textContent = input
+
 })
+
+var outputDiv = document.querySelector("#output")
 
 
  
