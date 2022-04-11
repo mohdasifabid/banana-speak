50d6b67d588541e0a27dd492be3e01cb1385fa02
asif <masifabid@outlook.com>
2022-04-11 14:02:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 var url = "https://api.funtranslations.com/translate/dolan.json"
-
-
 var input = document.querySelector("#textInput")
-console.log(heading.textContent)
-
 var output = document.querySelector("#textOutput")
-console.log(output.textContent)
-
 var btnTranslate = document.querySelector("#btnTranslate");
+var outputDiv = document.querySelector("#output")
 
-
-    btnTranslate.addEventListener("click", function () {
-           
-           var userInput = input.value;
-           fetch(url + "?text=" + userInput).then(function (response){
-               return response.json()
-           }).then(function (data){
-               console.log(data);
-               output.textContent = data.contents.translated;
-           })
-
+btnTranslate.addEventListener("click", function () {
+     var userInput = input.value;
+      fetch(url + "?text=" + userInput).then(function (response){
+          return response.json()
+        }).then(function (data){
+            output.textContent = data.contents.translated;
+        })
 })
 
-var outputDiv = document.querySelector("#output")
 
 
  
